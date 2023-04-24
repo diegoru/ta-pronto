fd1bc54f5cdc7273cf1fc9c3b4e2fa98334e2c1c
Diego <diego.ruescas@gmail.com>
2023-04-24 18:14:19 -0300

    conclusão loops

---
+++ hackerrank/src/introducion/javaloops2/Solution.java
@@ -0,0 +1,28 @@
+package introducion.javaloops2;
+
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public static void main(String[] argh) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for (int i = 0; i < t; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int res = a + b;
+            int controle = 1;
+
+            System.out.print(res);
+
+            for (int j = 1; j < n; j++) {
+                res = res + ((2 * controle) * b);
+                controle *= 2;
+                System.out.print(" " + res);
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
